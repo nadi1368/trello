ef48896b05c1050038bc6ba77c6cdb3dbc8004f2
Mahdi Heydari <mheydaridev@gmail.com>
2024-12-03 10:30:30 +0330

    convert phone to name members

--- src/views/project/_member_list.php
+++ src/views/project/_member_list.php
@@ -1,5 +1,5 @@
 <?php foreach($select_members as $member): ?>
     <label class="badge badge-info" title="<?= $member->user->first_name.' '.$member->user->last_name; ?>">
-        <?= $member->user->username; ?>
+        <?= $member->user->first_name.' '.$member->user->last_name; ?>
     </label>
 <?php endforeach; ?>
--- src/views/task/_member_list.php
+++ src/views/task/_member_list.php
@@ -3,6 +3,6 @@
 <?php endif; ?>
 <?php foreach($select_members as $member): ?>
         <label class="badge badge-info" title="<?= $member->user->first_name.' '.$member->user->last_name; ?>">
-            <?= $member->user->username; ?>
+            <?= $member->user->first_name.' '.$member->user->last_name; ?>
         </label>
 <?php endforeach; ?>
