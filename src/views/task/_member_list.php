1950c748afc3735843c122c160198009eab24035
nader.bahadori <nader.bahadorii@gmail.com>
2024-12-03 15:32:33 +0330

    task view

--- src/views/task/_member_list.php
+++ src/views/task/_member_list.php
@@ -7,6 +7,6 @@ use hesabro\trello\Module;
 <?php endif; ?>
 <?php foreach($select_members as $member): ?>
         <label class="badge badge-info" title="<?= $member->user->first_name.' '.$member->user->last_name; ?>">
-            <?= $member->user->username; ?>
+            <?= $member->user?->fullname; ?>
         </label>
 <?php endforeach; ?>
