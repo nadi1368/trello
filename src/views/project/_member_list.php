c7ab21d96906c3df90905fb0a312dd323910ba06
nader.bahadori <nader.bahadorii@gmail.com>
2024-12-03 15:42:46 +0330

    task view

--- src/views/project/_member_list.php
+++ src/views/project/_member_list.php
@@ -1,5 +1,5 @@
 <?php foreach($select_members as $member): ?>
-    <label class="badge badge-success" title="<?= $member->user->first_name.' '.$member->user->last_name; ?>">
+    <label class="badge badge-info" title="<?= $member->user->first_name.' '.$member->user->last_name; ?>">
         <?= $member->user?->fullname; ?>
     </label>
 <?php endforeach; ?>
