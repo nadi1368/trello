61d808180573eec9d1074b9541a1b1e0abc271db
Mahdi Heydari <mheydaridev@gmail.com>
2024-12-08 09:29:23 +0330

    move menu to start index page

--- src/views/project/index.php
+++ src/views/project/index.php
@@ -5,15 +5,14 @@ use yii\helpers\Html;
 
 <div class="ui <?= Html::encode($project->color); ?>" id="<?= 'main-board-'.$project->id ?>" data-color="<?= Html::encode($project->color); ?>">
     <nav class="navbar app">
+        <li>
+            <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-menu" title="<?= Module::t("module","Show Menu") ?>"><i class="fa fa-align-justify" data-pop-id="#pop-menu-menu"></i> </a>
+        </li>    
         <li role="presentation" class="dropdown" id="menu_archive_list">
             <?= $this->render('_menu_archive_list',['archive_statuses'=>$archive_statuses]); ?>
         </li>
-
         <li role="notification" class="dropdown" id="menu_notification_list">
             <?= $this->render('_menu_notification_list',['archive_statuses'=>$archive_statuses]); ?>
-        </li>
-        <li>
-            <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-menu" title="<?= Module::t("module","Show Menu") ?>"><i class="fa fa-align-justify" data-pop-id="#pop-menu-menu"></i> </a>
         </li>
         <li>
             <a href="#" class="icon-list" onclick="return showPopOverTeam(this);" data-pop-id="#pop-menu-team" title="<?= Module::t("module","Create New Team") ?>"><i class="fa fa-plus" data-pop-id="#pop-menu-team"></i> </a>
